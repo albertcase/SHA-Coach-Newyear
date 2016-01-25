910480086ffdd3b1952637a309dd4b991d175b19
Vincen Wen <vincen.wen@samesamechina.com>
2016-01-25 18:58:34 +0800

    no message

--- js/share.js
+++ js/share.js
@@ -1,3 +1,7 @@
+ <script type="text/javascript" src="http://wechatjs.curio.im/api/v1/js/9cd0fdb4-38a3-4f30-8bba-d685d556c213/wechat.js"></script>
+
+
+
 /* 微信分享 */
 
 
@@ -7,14 +11,14 @@ wx.ready(function(){
     window.wechat_setting.friend  = {
         title: 'COACH顽猴大闹照片，新年“猴猴”玩',
         desc: '有只调皮的猴子跑进我的照片里，太“猴”玩了！快看它有没有溜进你的照片里玩闹一番！',
-        link: window.location.host,
+        link: window.location.href,
         imgUrl: window.location.host + '/img/share.jpg', 
     };
 
     wx.onMenuShareAppMessage({
     title: 'COACH顽猴大闹照片，新年“猴猴”玩',
     desc: '有只调皮的猴子跑进我的照片里，太“猴”玩了！快看它有没有溜进你的照片里玩闹一番！',
-    link: window.location.host,
+    link: window.location.href,
     imgUrl: window.location.host + '/img/share.jpg', 
     type: '', 
     dataUrl: '', 
@@ -29,12 +33,12 @@ wx.ready(function(){
 
     window.wechat_setting.timeline = {
             title: 'COACH顽猴大闹照片，新年“猴猴”玩',
-            link: window.location.host,
+            llink: window.location.href,
             imgUrl: window.location.host + '/img/share.jpg', 
     };
     wx.onMenuShareTimeline({
         title: 'COACH顽猴大闹照片，新年“猴猴”玩',
-        link: window.location.host,
+        link: window.location.href,
         imgUrl: window.location.host + '/img/share.jpg', 
         success: function () { 
             _hmt.push(['_trackEvent', 'share', 'ShareTimeline']);
